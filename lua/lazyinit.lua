3a5b0d1fb6ea71a7a9cfcf36129d6f5abf790d36
johnson336 <lotjjohnson@gmail.com>
2024-03-23 14:56:42 -0700

    Silenced noisy Lua warnings

--- lua/lazyinit.lua
+++ lua/lazyinit.lua
@@ -436,7 +436,12 @@ lazy.setup({
                 callSnippet = 'Replace',
               },
               -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-              -- diagnostics = { disable = { 'missing-fields' } },
+              diagnostics = {
+                disable = {
+                  'missing-fields',
+                  'lowercase-global',
+                }
+              },
             },
           },
         },
